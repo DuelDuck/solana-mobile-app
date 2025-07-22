a458fd86b4e84c48c366477bc1012a6235124633
Max <Max@MacBook-Air---Max.local>
2025-07-22 12:23:30 +0300

    added fonts

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,27 @@
+import 'package:flutter/material.dart';
+
 class ProjectConstants {}
 
 class ProjectSource {}
 
-class ProjectFonts {}
+class ProjectFonts {
+  static const TextStyle headerRegular = TextStyle(
+    fontFamily: "Bungee",
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+  static const TextStyle bodySemibold = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+  static const TextStyle bodyRegular = TextStyle(
+    fontFamily: "Poppins",
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+}
 
 class ProjectColors {}
