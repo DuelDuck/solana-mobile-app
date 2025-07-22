ba0b5684226e82efd0f7bdefa0288308485943cc
Max <Max@MacBook-Air---Max.local>
2025-07-22 12:54:05 +0300

    added localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:duelduck_solana/ui/screens/main_screen.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en', 'EN')],
+      path: 'l10n',
+      fallbackLocale: const Locale('en', 'EN'),
+      child: const MyApp(),
+    ),
+  );
 }
